f4029c89b8ff37567ceec4b2ae38bb0d32fe6bb6
Luigi Matias <36388765+igiboi@users.noreply.github.com>
2023-03-19 18:34:09 +0000

    update my files under folder notes into javascript

---
+++ .build/notes/reverseString.js
@@ -0,0 +1,14 @@
+function reverse(str) {
+  if (!str || str.length < 2 || typeof str !== "string") {
+    return "hmm there is no string";
+  }
+  const backwards = [];
+  const totalItems = str.length - 1;
+  for (let i = totalItems; i = 0; i--) {
+    backwards.push(str[i]);
+  }
+  console.log(backwards);
+  return backwards.join("");
+}
+reverse("Hi my name is Luigi");
+//# sourceMappingURL=reverseString.js.map
